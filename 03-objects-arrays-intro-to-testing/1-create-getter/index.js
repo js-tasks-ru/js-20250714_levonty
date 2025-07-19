61bf45ec4f265f4245bdfe94673928d8c90c755f
levontium <levonty@yandex.ru>
2025-07-19 22:44:26 +0300

    return object value by given path

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,23 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+	return function(obj) {
+		const pathArr = path.split('.');
+		const lastKey = pathArr.pop();
+		let lastObj = pathArr.reduce((acc,index)=>{
+			if (acc.hasOwnProperty(index)) {
+				return acc[index];
+			} else {
+				return false;
+			}
+		
+		},obj);
+		
+		if (lastObj) {
+			if (lastObj.hasOwnProperty(lastKey)) {
+				return lastObj[lastKey];
+			} 
+		}
+		return;
+	}
 }
