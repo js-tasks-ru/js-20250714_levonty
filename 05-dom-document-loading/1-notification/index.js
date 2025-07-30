540db95aef04fe49b735467a37910a23915fd712
levontium <levonty@yandex.ru>
2025-07-30 13:15:25 +0300

    show notifications

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,74 @@
 export default class NotificationMessage {
+	
+	static isMsgShown = false;
+	
+	constructor(msg = '', {duration = 1000, type = 'success'} = {}) {
+		this.msg = msg;
+		this.duration = duration;
+		this.type = this.checkMsgType(type);
+		this.element = this.createMsg();
+		
+	}
+	
+	createMsg() {
+		
+		const div = document.createElement('tpl');
+			
+		div.innerHTML = this.msgTemplate();
+		return div.firstElementChild;
+		
+	}
+	
+	msgTemplate() {
+		return `
+			<div class="notification ${this.type}" style="--value:20s">
+				<div class="timer"></div>
+				<div class="inner-wrapper">
+				  <div class="notification-header">${this.type}</div>
+				  <div class="notification-body">
+					${this.msg}
+				  </div>
+				</div>
+			  </div>		
+		`;		
+	}
+	
+	remove() {
+		this.element.remove();
+		NotificationMessage.isMsgShown = false;
+	}
+	
+	destroy() {
+		this.element.remove();
+		NotificationMessage.isMsgShown = false;
+	}
+	
+	checkMsgType(type) {
+		let types = ['success','error'];
+		if (types.includes(type)) {
+			return type;
+		} else {
+			return types[0];
+		}
+	}
+	
+	show(node = document.body) {
+		let parentNode;
 
+		if (NotificationMessage.isMsgShown) return;
+		
+		if (typeof (node)=== 'string') {
+			parentNode = document.querySelector(node);
+
+		} else {
+			parentNode = node;		
+		}
+			
+		parentNode.append(this.element);
+
+		NotificationMessage.isMsgShown = true;
+		setTimeout(() => {
+			this.remove();
+		}, this.duration);
+	}
 }
