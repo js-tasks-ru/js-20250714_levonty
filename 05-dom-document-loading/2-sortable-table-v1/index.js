3651f96d9023dc8059f8bc68a7b8bdc728774238
levontium <levonty@yandex.ru>
2025-08-22 22:12:58 +0300

    sortable table v3

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,23 +1,82 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  constructor(headerConfig = [], { 
+	data = [], 
+	sorted = {}
+	} = {}) {
     this.header = headerConfig;
     this.data = data;
+	this.sorted = sorted;	
+	this.isSortLocally = true;
+
     this.element = this.createTableElement();
     this.subElements = {
       body: this.element.querySelector('[data-element="body"]'),
       header: this.element.querySelector('[data-element="header"]')
-    };
+    };	
 
+	if (this.sorted['id'] && this.sorted['order']) this.setSorting({id: this.sorted['id'], order: this.sorted['order']});
+
+	this.handleTheaderClick = this.handleTheaderClick.bind(this);
+	this.subElements.header.addEventListener('pointerdown',this.handleTheaderClick); //*
+	
   }
+  
+  handleTheaderClick(event) {
+	  const sortParams = this.defineSortColumn(event);
+	  if (sortParams) {
+			this.setSorting(sortParams);
+	  }
+  }
+  
+  defineSortColumn(event){
+
+	  const headerCell = event.target.closest('.sortable-table__cell');
+	  if (!headerCell) return;
+	  if (headerCell.dataset.sortable!=='true') return {};
+
+	  
+	  const order = headerCell.dataset.order ===  'desc' ? 'asc' : 'desc';
+	  headerCell.dataset.order = order;
+	  
+		return {
+			id: headerCell.dataset.id, 
+			order
+		}
+  }
+  
+ setSorting = ({id, order}) => {
+
+	 if (!id || !order) return
+
+	 const headerCell = this.subElements.header.querySelector(`[data-id="${id}"]`); 
+
+	 if (!headerCell) return;
+	 if (headerCell.dataset.sortable!='true') return;
+	  
+	  const arrow = this.subElements.header.querySelector('.sortable-table__sort-arrow');
+	  if (arrow) arrow.remove();
+	  const arrowElement = document.createElement('span');
+	  arrowElement.innerHTML = `<span class="sort-arrow"></span>`;
+		arrowElement.className = 'sortable-table__sort-arrow';
+		arrowElement.dataset.element = 'arrow';
+
+
+	  headerCell.append(arrowElement);
+	  	  
+	  this.sort(id, order); 
+	  headerCell.dataset.order =  order;
+ }
  
+
+	
   createTableHeaderElement() {
-	  const headerRow = this.header.map(item => (`<div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}">
+
+	  const headerRow = this.header.map(item => (`<div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}" data-order="">
         <span>${item.title}</span>
       </div>`)).join('');
 	  return `<div data-element="header" class="sortable-table__header sortable-table__row">${headerRow}</div>`;
   }
   
-
   
   createTableBodyElement() {
 	  const tableBody = this.data.map(item => this.createRowElement(item)).join('');
@@ -68,9 +127,10 @@ export default class SortableTable {
   }
 	
   sort(field, order) {
+	  
 	  let sortType = null;
 	  let sorted;
-	  let sortDir = order == 'asc' ? 1 : -1;
+	  let sortDir = order === 'asc' ? 1 : -1;
 
 	  for (let obj of this.header) {
 		  if (obj['id'] == field) {
@@ -78,25 +138,30 @@ export default class SortableTable {
 			  sortType = obj['sortType'];
 			  if (sortType == 'number') {
 				  this.data.sort((rowA, rowB)=>sortDir * (Number(rowA[field]) - Number(rowB[field])));
-			  } else if (sortType == 'string') {
-				 this.data.sort((rowA, rowB)=>sortDir * (rowA[field].localeCompare(rowB[field], ["ru", "en"], 
-					  { 
-              sensitivity: "case", // Учитывать регистр
-              caseFirst: "upper", // Сначала заглавные буквы
-              numeric: true // Для числовых строк (опционально)
-					  }))); 
+			  } else if (sortType === 'string') {
+				 this.data.sort(
+					(rowA, rowB)=> { 
+						let compare = String(rowA[field]).localeCompare(String(rowB[field]), 
+							["ru", "en"], 
+							{ sensitivity: "variant" }
+						);
+						return sortDir===1 ? compare: -compare;
+					}
+				);
+				
 			  }
 			  break;
 		  }
 	  }
 	  
-
-	  const oldTableBody = document.querySelector('[data-element="body"]');
+		const oldTableBody = this.subElements.body;
+	  
 	  oldTableBody.innerHTML = this.createTableBodyElement();
 	  
   }	
   
   destroy() {
+	  this.subElements.header.removeEventListener('pointerdown',this.handleTheaderClick);
 	  this.element.remove();
   }
   
